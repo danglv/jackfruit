d29cb9fb28d7ebc00fd7911ec65b047945a2cc2d
luandt <luandt@topica.edu.vn>
2015-07-08 19:37:40 +0700

    feat(course): add layout list-course

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -10,9 +10,3 @@
 // Read Sprockets README (https://github.com/rails/sprockets#sprockets-directives) for details
 // about supported directives.
 //
-//= require jquery
-//= require materialize-sprockets
-//= require jquery_ujs
-//= require turbolinks
-//= require hogan.js
-//= require_tree ./app
