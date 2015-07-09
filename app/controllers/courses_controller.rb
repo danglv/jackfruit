6726ab0c922611688a2d8eea933b025e1451d76d
luandt <luandt@topica.edu.vn>
2015-07-09 11:33:38 +0700

    feat(course): add layout course and course-search

--- app/controllers/courses_controller.rb
+++ app/controllers/courses_controller.rb
@@ -1,14 +1,14 @@
 class CoursesController < ApplicationController
 
   def index
-    category_name = params[:category]
-    category = Category.where(name: category_name).first
+    # category_name = params[:category]
+    # category = Category.where(name: category_name).first
 
-    if category.blank?
-      @courses = Course.all
-    else
-      @courses = Course.where(:category_ids.in => [category.id])
-    end
+    # if category.blank?
+    #   @courses = Course.all
+    # else
+    #   @courses = Course.where(:category_ids.in => [category.id])
+    # end
   end
 
   def show
@@ -17,12 +17,12 @@ class CoursesController < ApplicationController
   end
 
   def search
-    keywords = params[:q]
-    pattern = /#{Regexp.escape(keywords)}/
-    @course = Course.where(name: pattern).limit(10)
+    # keywords = params[:q]
+    # pattern = /#{Regexp.escape(keywords)}/
+    # @course = Course.where(name: pattern).limit(10)
 
-    if @course.count == 0
-      @course = Course.where(description: pattern).limit(10)
-    end
+    # if @course.count == 0
+    #   @course = Course.where(description: pattern).limit(10)
+    # end
   end
 end
