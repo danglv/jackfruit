cfcb3c368b6a88267d7d735d779a08e76b3fe0e0
luandt <luandt@topica.edu.vn>
2015-07-11 09:45:59 +0700

    feat(course): fix html

--- app/controllers/courses_controller.rb
+++ app/controllers/courses_controller.rb
@@ -1,34 +1,34 @@
 class CoursesController < ApplicationController
 
   def index
-    category_name = params[:category]
-    category = Category.where(name: category_name).first
+    # category_name = params[:category]
+    # category = Category.where(name: category_name).first
 
-    if category.blank?
-      @courses = Course.all
-    else
-      @courses = Course.where(:category_ids.in => [category.id])
-    end
+    # if category.blank?
+    #   @courses = Course.all
+    # else
+    #   @courses = Course.where(:category_ids.in => [category.id])
+    # end
 
-    head :ok
+    # head :ok
   end
 
   def show
-    course_id = params[:id]
-    @course = Course.where(id: course_id).first
+    # course_id = params[:id]
+    # @course = Course.where(id: course_id).first
     
-    head :ok
+    # head :ok
   end
 
   def search
-    keywords = params[:q]
-    pattern = /#{Regexp.escape(keywords)}/
-    @course = Course.where(name: pattern).limit(10)
+    # keywords = params[:q]
+    # pattern = /#{Regexp.escape(keywords)}/
+    # @course = Course.where(name: pattern).limit(10)
 
-    if @course.count == 0
-      @course = Course.where(description: pattern).limit(10)
-    end
+    # if @course.count == 0
+    #   @course = Course.where(description: pattern).limit(10)
+    # end
 
-    head :ok
+    # head :ok
   end
 end
