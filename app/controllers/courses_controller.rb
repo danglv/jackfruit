03cfa725f18f857bdfd08183f57ff823b82fe682
Thanh Nguyen <thanhnv2@topica.edu.vn>
2015-07-17 21:09:25 +0700

    do du lieu learning & lecture

--- app/controllers/courses_controller.rb
+++ app/controllers/courses_controller.rb
@@ -99,7 +99,7 @@ class CoursesController < ApplicationController
     end
 
     @lecture = @course.curriculums.where(:lecture_index => lecture_index).first
-    @owned_course = @current_user.courses.where(course_id: course_id).first
+    @owned_course = current_user.courses.where(course_id: course_id).first
 
     # set lecture ratio = 100(finish)
     @owned_lecture = @owned_course.lectures.where(lecture_index: lecture_index).first
