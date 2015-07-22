a158b274cd41cebd5d1561c781620c00105a2e79
DuongNguyen <duyduongzqa@gmail.com>
2015-07-22 19:58:56 +0700

    sua text cho ben su pham

--- config/application.rb
+++ config/application.rb
@@ -27,7 +27,7 @@ module Jackfruit
     # config.time_zone = 'Central Time (US & Canada)'
 
     # config I18n vi
-    config.i18n.default_locale = :en
+    config.i18n.default_locale = :vi
 
     # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
