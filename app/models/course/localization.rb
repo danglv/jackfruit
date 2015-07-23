294ba0cfd0093931686efcf629ce3ea7daee9bed
La Van Dang <danglv@topica.edu.vn>
2015-07-23 11:05:43 +0700

    Sua localize trong trang detail

--- app/models/course/localization.rb
+++ app/models/course/localization.rb
@@ -21,4 +21,34 @@ module Course::Localization
       :en => "Related Course",
     }
   }
+
+  LEVELS = {
+    :all => {
+      :vi => "Tất cả",
+      :en => "All"
+    },
+    :beginner => {
+      :vi => "Sơ cấp",
+      :en => "Beginner"
+    },
+    :intermediate => {
+      :vi => "Trung cấp",
+      :en => "Intermediate"
+    },
+    :expert => {
+      :vi => "Cao cấp",
+      :en => "Expert"
+    }
+  }
+
+  LANGUAGE = {
+    :en => {
+      :vi => "Vietnamese",
+      :en => "English"
+    },
+    :vi => {
+      :vi => "Tiếng Việt",
+      :en => "Tiếng Anh"
+    }
+  }
 end
