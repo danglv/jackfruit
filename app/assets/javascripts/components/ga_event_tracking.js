ab83e163fafe588742d49e57911f502044a00b6e
Nguyễn Phan Hải <hainp2604@gmail.com>
2015-07-24 17:17:29 +0700

    fix: disable tracking event

--- app/assets/javascripts/components/ga_event_tracking.js
+++ app/assets/javascripts/components/ga_event_tracking.js
@@ -3,66 +3,66 @@
 $(document).ready(function () {
   if (ga) {
     // Popup login tracking
-    $('#modal-login .btn-facebook').click(function (e) {
-      e.preventDefault();
-      e.stopPropagation();
-      ga('send', 'event', 'login-modal', 'login', 'Facebook login', {
-        hitCallback: function () {
-          window.location = '/users/auth/facebook';
-        }
-      });
-    });
+    // $('#modal-login .btn-facebook').click(function (e) {
+    //   e.preventDefault();
+    //   e.stopPropagation();
+    //   ga('send', 'event', 'login-modal', 'login', 'Facebook login', {
+    //     hitCallback: function () {
+    //       window.location = '/users/auth/facebook';
+    //     }
+    //   });
+    // });
 
-    $('#modal-login .btn-google').click(function (e) {
-      e.preventDefault();
-      e.stopPropagation();
-      ga('send', 'event', 'login-modal', 'login', 'Google login', {
-        hitCallback: function () {
-          window.location = '/users/auth/google_oauth2';
-        }
-      });
-    });
+    // $('#modal-login .btn-google').click(function (e) {
+    //   e.preventDefault();
+    //   e.stopPropagation();
+    //   ga('send', 'event', 'login-modal', 'login', 'Google login', {
+    //     hitCallback: function () {
+    //       window.location = '/users/auth/google_oauth2';
+    //     }
+    //   });
+    // });
 
-    $('#modal-login input[type="submit"]').click(function (e) {
-      e.preventDefault();
-      e.stopPropagation();
-      ga('send', 'event', 'login-modal', 'login', 'Email login', {
-        hitCallback: function () {
-          $('#modal-login form').submit();
-        }
-      });
-    });
+    // $('#modal-login input[type="submit"]').click(function (e) {
+    //   e.preventDefault();
+    //   e.stopPropagation();
+    //   ga('send', 'event', 'login-modal', 'login', 'Email login', {
+    //     hitCallback: function () {
+    //       $('#modal-login form').submit();
+    //     }
+    //   });
+    // });
 
-    // Popup register tracking
-    $('#modal-register .btn-facebook').click(function (e) {
-      e.preventDefault();
-      e.stopPropagation();
-      ga('send', 'event', 'register-modal', 'register', 'Facebook register', {
-        hitCallback: function () {
-          window.location = '/users/auth/facebook';
-        }
-      });
-    });
+    // // Popup register tracking
+    // $('#modal-register .btn-facebook').click(function (e) {
+    //   e.preventDefault();
+    //   e.stopPropagation();
+    //   ga('send', 'event', 'register-modal', 'register', 'Facebook register', {
+    //     hitCallback: function () {
+    //       window.location = '/users/auth/facebook';
+    //     }
+    //   });
+    // });
 
-    $('#modal-register .btn-google').click(function (e) {
-      e.preventDefault();
-      e.stopPropagation();
-      ga('send', 'event', 'register-modal', 'register', 'Google register', {
-        hitCallback: function () {
-          window.location = '/users/auth/google_oauth2';
-        }
-      });
-    });
+    // $('#modal-register .btn-google').click(function (e) {
+    //   e.preventDefault();
+    //   e.stopPropagation();
+    //   ga('send', 'event', 'register-modal', 'register', 'Google register', {
+    //     hitCallback: function () {
+    //       window.location = '/users/auth/google_oauth2';
+    //     }
+    //   });
+    // });
 
-    $('#modal-register input[type="submit"]').click(function (e) {
-      e.preventDefault();
-      e.stopPropagation();
-      ga('send', 'event', 'register-modal', 'register', 'Email register', {
-        hitCallback: function () {
-          $('#modal-register form').submit();
-        }
-      });
-    });
+    // $('#modal-register input[type="submit"]').click(function (e) {
+    //   e.preventDefault();
+    //   e.stopPropagation();
+    //   ga('send', 'event', 'register-modal', 'register', 'Email register', {
+    //     hitCallback: function () {
+    //       $('#modal-register form').submit();
+    //     }
+    //   });
+    // });
 
     // $('#btn-buy').click(function (e) {
     //   var link = $('#btn-buy').attr('href');
