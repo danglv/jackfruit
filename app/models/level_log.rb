552cdc01ff06a3b927e2f0f905f618c2261cc963
Đăng LV <danglv@topica.edu.vn>
2015-07-29 14:55:50 +0700

    refine

--- app/models/level_log.rb
+++ app/models/level_log.rb
@@ -6,7 +6,6 @@ class LevelLog
 
   field :level_before_up, type: String, default: Constants.UserLevelValues.first
   field :level_after_up, type: String, default: Constants.UserLevelValues.first
-  field :platform, type: String
   
   validates_inclusion_of :level_before_up, :level_after_up, :in => Constants.UserLevelValues
   default_scope -> {asc(:created_at)}
