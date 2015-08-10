c359adc0f851e994506f11bf93d0bc4e29f62911
Đăng LV <danglv@topica.edu.vn>
2015-08-10 12:02:37 +0700

    Thêm trạng thái thanh toán payment và cập nhật owned_course theo trạng thái thanh toán của payment

--- app/models/payment.rb
+++ app/models/payment.rb
@@ -12,6 +12,30 @@ class Payment
   field :city, type: String, default: ""
   field :district, type: String, default: ""
   field :method, type: String, default: Constants.PaymentMethodValues.first
+  field :status, type: String, default: Constants::PaymentStatus::PENDING
 
   validates_inclusion_of :method, :in => Constants.PaymentMethodValues
+
+  before_save :update_status
+  before_destroy :check_owned_course
+
+  def status_enum
+    Constants.PaymentStatusValues
+  end
+
+  def update_status
+    current_user = self.user
+
+    if self.status == Constants::PaymentStatus::CANCEL
+      current_user.courses.where(course_id: self.course_id).destroy
+    else
+      owned_course = current_user.courses.where(course_id: self.course_id).first
+      owned_course.set(payment_status: self.status) unless owned_course.blank?
+    end
+  end
+
+  def check_owned_course
+    current_user = self.user
+    current_user.courses.where(course_id: self.course_id).destroy
+  end
 end
