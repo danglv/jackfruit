a277454c04bc7bed5b79606de5a351bbae53b1ab
Nguyễn Phan Hải <hainp2604@gmail.com>
2015-08-19 11:32:23 +0700

    test(payment): refactor payment tests

--- test/controllers/concerns/payment_services_test.rb
+++ test/controllers/concerns/payment_services_test.rb
@@ -1,4 +1,5 @@
 require 'test_helper'
+require 'json'
 
 class FakePaymentController < ApplicationController
   include PaymentServices
@@ -6,7 +7,7 @@ end
 
 class FakePaymentControllerTest < ActionController::TestCase
   def setup
-    @baokim = FakePaymentController::BaoKimPayment.new
+    @baokim = FakePaymentController::BaoKimPaymentPro.new
   end
 
   def teardown
@@ -15,75 +16,5 @@ class FakePaymentControllerTest < ActionController::TestCase
   
   test 'should have BaoKimPayment declared' do
     assert @baokim
-  end
-
-  test 'should have function create_request_url that accepts a hash as argument' do
-    assert @baokim.create_request_url({
-      'order_id' =>  '',
-      'business' =>  '',
-      'total_amount' =>  '',
-      'shipping_fee' =>  '',
-      'tax_fee' =>  '',
-      'order_description' =>  '',
-      'url_success' =>  '',
-      'url_cancel' =>  '',
-      'url_detail' =>  ''
-    })
-  end
-
-  test 'create_request_url should return an url which has one question mark' do
-    params = {
-      'order_id' =>  '',
-      'business' =>  '',
-      'total_amount' =>  '',
-      'shipping_fee' =>  '',
-      'tax_fee' =>  '',
-      'order_description' =>  '',
-      'url_success' =>  '',
-      'url_cancel' =>  '',
-      'url_detail' =>  ''
-    }
-
-    url = @baokim.create_request_url(params)
-
-    assert_equal  1, url.count('?')
-  end
-
-  test 'create_request_url should return an url which is formatted' do
-    params = {
-      'order_id' =>  1,
-      'business' =>  'ngoc.phungba@gmail.com',
-      'total_amount' =>  1,
-      'shipping_fee' =>  0,
-      'tax_fee' =>  0,
-      'order_description' =>  '',
-      'url_success' =>  '',
-      'url_cancel' =>  '',
-      'url_detail' =>  ''
-    }
-
-    url = @baokim.create_request_url(params)
-
-    assert url.index('merchant_id') > 0
-    assert url.index('business=ngoc.phungba@gmail.com') > 0
-    assert url.index('92F225C50E447B66F65531F8F43BACB5') > 0
-  end
-
-  test 'verify_response_url should be truthy with same params' do
-    params = {
-      'merchant_id' => '18578',
-      'order_id' =>  1,
-      'business' =>  'ngoc.phungba@gmail.com',
-      'total_amount' =>  1,
-      'shipping_fee' =>  0,
-      'tax_fee' =>  0,
-      'order_description' =>  '',
-      'url_success' =>  '',
-      'url_cancel' =>  '',
-      'url_detail' =>  '',
-      'checksum' => '92F225C50E447B66F65531F8F43BACB5'
-    }
-
-    assert @baokim.verify_response_url(params)    
   end
 end
