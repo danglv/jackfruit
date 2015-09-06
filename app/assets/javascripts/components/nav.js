7addca84de3a0999a4e27d9a04fa9249d162e342
Nguyễn Phan Hải <hainp2604@gmail.com>
2015-09-06 13:53:21 +0700

    refactor: add slide sidebar open event handler

--- app/assets/javascripts/components/nav.js
+++ app/assets/javascripts/components/nav.js
@@ -2,48 +2,49 @@
   'use strict';
 
   $.fn.sliderDropdown = function () {
+    var ANIMATED_DURATION = 200;
     var activator = this;
-
-    this.click(function () {
-
-      // get pushedContainer and content 
-      var dropdownContainer = $($(this).attr("data-dropdown-container"));
-      //  prepend content into pushedContainer
-      dropdownContainer.css({
-        "float": "left"
-      });
-      // show or hide slide
-      var isOpen = activator.parent().attr('class').indexOf('open') >= 0;
-
-      if (isOpen) {
-        dropdownContainerCloseAnimationHandler(dropdownContainer);
-      } else {
-        dropdownContainer.animate({
-          "margin-left": "0px"
-        }, {
-          "duration": 200,
-          "done": function () {
-            activator.parent().addClass('open');
-            activator.parent().find('.dropdown-backdrop').click(function () {
-              dropdownContainerCloseAnimationHandler(dropdownContainer);
-            });
-          }
-        });
-      }
-    });
 
     function dropdownContainerCloseAnimationHandler(dropdownContainer) {
       dropdownContainer.animate({
-        "margin-left": "-" + dropdownContainer.width() + "px",
+        'margin-left': '-' + dropdownContainer.width() + 'px',
       }, {
-        "duration": 200,
-        "done": function () {
+        'duration': ANIMATED_DURATION,
+        'done': function () {
           activator.parent().removeClass('open');
         }
       });
 
       dropdownContainer.removeClass('open');
     }
+
+    function dropdownContainerOpenAnimationHandler(dropdownContainer) {
+      dropdownContainer.animate({
+        'margin-left': '0px'
+      }, {
+        'duration': ANIMATED_DURATION,
+        'done': function () {
+          activator.parent().addClass('open');
+          activator.parent().find('.dropdown-backdrop').click(function () {
+            dropdownContainerCloseAnimationHandler(dropdownContainer);
+          });
+        }
+      });
+    }
+
+    this.click(function () {
+      // get pushedContainer and content 
+      var dropdownContainer = $($(this).attr('data-dropdown-container'));
+
+      // show or hide slide
+      var isOpen = activator.parent().attr('class').indexOf('open') >= 0;
+
+      if (isOpen) {
+        dropdownContainerCloseAnimationHandler(dropdownContainer);
+      } else {
+        dropdownContainerOpenAnimationHandler(dropdownContainer);
+      }
+    });
 
     return this;
   };
