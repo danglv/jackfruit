3124de57ea13cacb853a2d3a9030957727a18521
Thanh Nguyen <thanhnv2@topica.edu.vn>
2015-09-08 17:27:22 +0700

    Change link send noti for flow

--- app/controllers/payment_controller.rb
+++ app/controllers/payment_controller.rb
@@ -73,7 +73,7 @@ class PaymentController < ApplicationController
       if payment.save
         create_course_for_user()
         begin
-          RestClient.post 'http://internal.tudemy.vn:8000/notify', :to => 'mercury', :msg => "{type:'cod', msg: 'Có đơn COD mới'}}"
+          RestClient.post 'http://localhost:8000/notify/cod/create', :type => 'cod', :payment => :json => payment, :msg => 'Có đơn COD mới', 
         rescue => e
         end
         redirect_to root_url + "/home/payment/#{payment.id.to_s}/pending?alias_name=#{@course.alias_name}"
