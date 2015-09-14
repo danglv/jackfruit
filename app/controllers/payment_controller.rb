d43ff3732f2270bff0e3cfa2f7de4c46124bd9ad
Thanh Nguyen <thanhnv2@topica.edu.vn>
2015-09-14 16:25:02 +0700

    Change address flow server in jackfruit

--- app/controllers/payment_controller.rb
+++ app/controllers/payment_controller.rb
@@ -75,7 +75,7 @@ class PaymentController < ApplicationController
       if payment.save
         create_course_for_user()
         begin
-          RestClient.post 'http://localhost:8000/notify/cod/create', :type => 'cod', :payment => payment.as_json, :msg => 'Có đơn COD mới' 
+          RestClient.post 'http://flow.pedia.vn:8000/notify/cod/create', :type => 'cod', :payment => payment.as_json, :msg => 'Có đơn COD cần xử lý ' 
         rescue => e
         end
         redirect_to root_url + "/home/payment/#{payment.id.to_s}/pending?alias_name=#{@course.alias_name}"
