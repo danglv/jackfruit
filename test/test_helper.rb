9469710a107e67a3988cb929016b90e6cabcb749
Nguyễn Phan Hải <hainp2604@gmail.com>
2015-09-15 19:38:32 +0700

    test(authentication): use PhantomJS instead of Chrome

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -4,7 +4,7 @@ require 'rails/test_help'
 require 'minitest/rails'
 require 'minitest/rails/capybara'
 require 'capybara/rails'
-# require 'capybara/poltergeist'
+require 'capybara/poltergeist'
 
 require "minitest/reporters"
 
@@ -31,16 +31,16 @@ class ActiveSupport::TestCase
   # Capybara.default_driver = :selenium
 
   ## Test with Chrome/Chromium
-  Capybara.register_driver :chrome do |app|
-    Capybara::Selenium::Driver.new(app, {:browser => :chrome})
-  end
-  Capybara.default_driver = :chrome
+  # Capybara.register_driver :chrome do |app|
+  #   Capybara::Selenium::Driver.new(app, {:browser => :chrome})
+  # end
+  # Capybara.default_driver = :chrome
 
   ## Test with phantomjs
-  # Capybara.register_driver :poltergeist do |app|
-  #   Capybara::Poltergeist::Driver.new(app, {debug: true, js_errors:true, phantomjs_options:['--proxy-type=none'], timeout:180})
-  # end
-  # Capybara.default_driver = :poltergeist
+  Capybara.register_driver :poltergeist do |app|
+    Capybara::Poltergeist::Driver.new(app, {debug: true, js_errors:true, phantomjs_options:['--proxy-type=none'], timeout:180})
+  end
+  Capybara.default_driver = :poltergeist
 
   ## Test with phantomjs and debug mode enabled
   # Capybara.register_driver :poltergeist_debug do |app|
