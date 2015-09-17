8e66ffe4e4d98a23146a559062228ee9b238aeb6
luandt <luandt@topica.edu.vn>
2015-09-17 19:48:28 +0700

    add text recently discussion

--- app/assets/javascripts/components/lecture.js
+++ app/assets/javascripts/components/lecture.js
@@ -63,8 +63,8 @@
         data: params,
         success: function(msg){
           var data = msg;
-
-          var discussionItem = "<div class='row discussion-item no-margin'> <div class='col-md-1 col-lg-1 no-padding discussion-item-avatar'> <i class='fa fa-smile-o'></i> </div> <div class='col-md-11 col-lg-11 no-padding discussion-item-main'> <ul class='discussion-item-title'> <li class='bold'>"+data.email+"</li> </ul> <span class='discussion-item-subject'>"+data.title+" </span> <p class='discussion-item-content'>"+data.description+" </p> </div> </div>";
+            
+          var discussionItem = "<div class='row discussion-item no-margin'> <div class='col-md-1 col-lg-1 no-padding discussion-item-avatar'> <i class='fa fa-smile-o'></i> </div> <div class='col-md-11 col-lg-11 no-padding discussion-item-main'> <ul class='discussion-item-title'> <li class='bold'>"+data.email+"</li><li>vừa đăng thảo luận</li> </ul> <span class='discussion-item-subject'>"+data.title+" </span> <p class='discussion-item-content'>"+data.description+" </p> </div> </div>";
           $(obj).parent().parent().parent().find(".list-discussion").prepend(discussionItem);
         }
       });
