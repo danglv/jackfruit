ab4c129864a335ac2aec26ccef7373297efe51ec
hoptq MBP <hoptq@topica.edu.vn>
2015-09-18 14:36:28 +0700

    Fix select wishlist stop reload courses page

--- app/assets/javascripts/components/wishlist.js
+++ app/assets/javascripts/components/wishlist.js
@@ -15,11 +15,19 @@
   //       }
   //     });
   // })
+  
   $.fn.wishlist = function () {
     this.click( function () {
       var course_id = $(this).attr('val');
       var params = {
         'course_id': course_id,
+      }
+
+      var heart = $(this).children().children().get(1);
+      if (heart.classList.contains("wishlist")){
+        heart.classList.remove("wishlist");
+      }else{
+        heart.classList.add("wishlist"); 
       }
 
       var URL = 'http://' + window.location.host + '/home/my-course/update_wishlist';
@@ -30,8 +38,7 @@
           success: function(msg){
           }
         });
-    })
-
+      })
   };
 
   $('.wishlist-heart').wishlist();
