d73261679cb95f586fe56cb81c6c68defd8428b7
luandt <luandt@topica.edu.vn>
2015-09-25 21:27:53 +0700

    fix header

--- app/assets/javascripts/components/payment.js
+++ app/assets/javascripts/components/payment.js
@@ -25,4 +25,47 @@ $(document).ready(function () {
     });
   };
   $(".select-card").selectmenthod();
+
+  $(".send-form-support").click(function () {
+
+    var course_id = $(".course_id").val();
+    var course_name = $(".course_name").val();
+    var user_id = $(".user_id").val();
+    var user_email = $(".user_email").val();
+    var name = $(".name-input").val();
+    var mobile = $(".mobile-input").val();
+
+
+    var params = {
+      'course_id': course_id,
+      'course_name' : course_name,
+      'user_id': user_id,
+      'email' : user_email,
+      'name': name,
+      'mobile': mobile,
+      'type' : 'course_page_support',
+      'msg' : name + ' cần hỗ trợ tại khóa học ' + course_name,
+      // 'payment_id'
+    }
+
+    var URL = 'http://flow.pedia.vn:8000/notify/course_page_support/create';
+    var request = $.ajax({
+        url: URL,
+        type: "post",
+        data: params,
+    });
+
+    // Callback handler that will be called on success
+    request.done(function (response, textStatus, jqXHR){
+        // Log a message to the console
+        $(".tele-sales-content").css("display", "none");
+        $(".tele-sales-extend .success").css("display", "block");
+    });
+
+    // Callback handler that will be called on failure
+    request.fail(function (jqXHR, textStatus, errorThrown){
+        // Log the error to the console
+    });
+  });
+  
 }); 
