a0bc818ba3eb59c527308beb80646ad88d8f510f
Nguyễn Phan Hải <hainp2604@gmail.com>
2015-09-29 01:11:05 +0700

    fix(payment): update test
    - add 2 test cases to cover phone card payment
    - after `after` block

--- test/features/payment_test.rb
+++ test/features/payment_test.rb
@@ -25,6 +25,10 @@ feature 'Payment' do
                 )
   end
 
+  after do
+    User.where(email: 'student1@tudemy.vn').first.courses.destroy_all
+  end
+
   scenario '[JPA001]' do
     if Capybara.current_session.driver.browser.respond_to? 'manage'
       Capybara.current_session.driver.browser.manage.window.resize_to(1280, 800)
@@ -35,28 +39,7 @@ feature 'Payment' do
     find('.buy-button').click
 
     within('#login-modal') do
-      fill_in('user[email]', with: 'nguyendanhtu@tudemy.vn')
-      fill_in('user[password]', with: '12345678')
-      find('.btn-login-submit').click
-    end
-
-    page.must_have_content('Test Course 1')
-    page.must_have_content('99,500')
-    page.must_have_content('199,000')
-    page.must_have_content('50%')
-  end
-
-  scenario '[JPA001.1]' do
-    if Capybara.current_session.driver.browser.respond_to? 'manage'
-      Capybara.current_session.driver.browser.manage.window.resize_to(1280, 800)
-    end
-
-    visit '/courses/test-course-1/detail?coupon_code=A_VALID_COUPON'
-
-    find('.buy-button').click
-
-    within('#login-modal') do
-      fill_in('user[email]', with: 'nguyendanhtu@tudemy.vn')
+      fill_in('user[email]', with: 'student1@tudemy.vn')
       fill_in('user[password]', with: '12345678')
       find('.btn-login-submit').click
     end
@@ -84,7 +67,7 @@ feature 'Payment' do
     find('.buy-button').click
 
     within('#login-modal') do
-      fill_in('user[email]', with: 'nguyendanhtu@tudemy.vn')
+      fill_in('user[email]', with: 'student1@tudemy.vn')
       fill_in('user[password]', with: '12345678')
       find('.btn-login-submit').click
     end
@@ -93,5 +76,49 @@ feature 'Payment' do
     page.must_have_content('99,500')
     page.must_have_content('199,000')
     page.must_have_content('50%')
+  end
+
+  scenario '[JPA003] phone card payment success' do
+    stub_request(:post, "https://www.baokim.vn/the-cao/restFul/send")
+      .to_return(:status => 200, :body => '{"amount": 199000}', :headers => {})
+
+    visit '/home/payment/card/test-course-1?p=baokim_card'
+
+    within('#login-modal') do
+      fill_in('user[email]', with: 'nguyendanhtu@tudemy.vn')
+      fill_in('user[password]', with: '12345678')
+      find('.btn-login-submit').click
+    end
+
+    within('.form-card') do
+      fill_in('pin_field', with: 34903384924074)
+      fill_in('seri_field', with: 36108200046632)
+      find('.purchase-button').click
+    end
+
+    page.must_have_content('Thành công')
+  end
+
+  scenario '[JPA004] phone card payment fail' do
+    stub_request(:post, "https://www.baokim.vn/the-cao/restFul/send")
+      .to_return(:status => 406,
+                 :body => '{"errorMessage": "Error", "transaction_id": 1}',
+                 :headers => {})
+
+    visit '/home/payment/card/test-course-1?p=baokim_card'
+
+    within('#login-modal') do
+      fill_in('user[email]', with: 'nguyendanhtu@tudemy.vn')
+      fill_in('user[password]', with: '12345678')
+      find('.btn-login-submit').click
+    end
+
+    within('.form-card') do
+      fill_in('pin_field', with: 34903384924074)
+      fill_in('seri_field', with: 36108200046632)
+      find('.purchase-button').click
+    end
+
+    page.must_have_content('Error')
   end
 end
