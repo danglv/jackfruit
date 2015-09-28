d2cc4778a1d4e1a531351dc245bc09b8535f9681
Nguyễn Phan Hải <hainp2604@gmail.com>
2015-09-28 11:34:01 +0700

    refactor(payment): refactor test

--- test/features/payment_test.rb
+++ test/features/payment_test.rb
@@ -2,7 +2,7 @@ require 'test_helper'
 
 feature 'Payment' do
   before do
-    stub_request(:get, "http://code.pedia.vn/coupon?coupon=TEST_COUPON")
+    stub_request(:get, "http://code.pedia.vn/coupon?coupon=A_VALID_COUPON")
       .with(:headers => {
         'Accept'=>'*/*; q=0.5, application/xml',
         'Accept-Encoding'=>'gzip, deflate',
@@ -12,7 +12,7 @@ feature 'Payment' do
       .to_return(:status => 200,
                  :body => [
                     '{"_id": "56027caa8e62a475a4000023"',
-                    '"coupon": "TEST_COUPON"',
+                    '"coupon": "A_VALID_COUPON"',
                     '"created_at": "2015-09-23T10:19:22.973Z"',
                     '"expired_date": "2015-09-23T17:00:00.000Z"',
                     '"course_id": "55cb2d3044616e15ca000000"',
@@ -31,7 +31,28 @@ feature 'Payment' do
       Capybara.current_session.driver.browser.manage.window.resize_to(1280, 800)
     end
 
-    visit '/courses/test-course-1/detail?coupon_code=TEST_COUPON'
+    visit '/courses/test-course-1/detail?coupon_code=A_VALID_COUPON'
+
+    find('.buy-button').click
+
+    within('#login-modal') do
+      fill_in('user[email]', with: 'nguyendanhtu@tudemy.vn')
+      fill_in('user[password]', with: '12345678')
+      find('.btn-login-submit').click
+    end
+
+    page.must_have_content('Test Course 1')
+    page.must_have_content('99,500')
+    page.wont_have_content('199,000')
+    page.wont_have_content('50%')
+  end
+
+  scenario '[JPA001.1]' do
+    if Capybara.current_session.driver.browser.respond_to? 'manage'
+      Capybara.current_session.driver.browser.manage.window.resize_to(1280, 800)
+    end
+
+    visit '/courses/test-course-1/detail?coupon_code=A_VALID_COUPON'
 
     find('.buy-button').click
 
@@ -57,7 +78,7 @@ feature 'Payment' do
     find('#coupon-dropdown-button').click
 
     within('#coupon-dropdown') do
-      fill_in('coupon_code', :with => 'TEST_COUPON')
+      fill_in('coupon_code', :with => 'A_VALID_COUPON')
       find('.coupon-submit').click
     end
 
