ae38a51b461c2519794fe6aafe6bedfca0363894
luandt <luandt@topica.edu.vn>
2015-09-29 15:13:04 +0700

    fix css

--- app/assets/javascripts/components/course_detail.js
+++ app/assets/javascripts/components/course_detail.js
@@ -18,38 +18,38 @@
     });
   };
 
-  // $.fn.documentScroll = function () {
-  //   // get hash url and set active
-  //   var hash = window.location.hash;
-  //   if (hash.trim() != "") {
-  //     $(".active").removeClass("active");
-  //     $(".menu-fixed a[href=" + hash + "]").parent().addClass("active");
-  //   }
+  $.fn.documentScroll = function () {
+    // get hash url and set active
+    var hash = window.location.hash;
+    if (hash.trim() != "") {
+      $(".active").removeClass("active");
+      $(".menu-fixed a[href=" + hash + "]").parent().addClass("active");
+    }
 
-  //   if ($(document).scrollTop() >= $($(".description")[4]).offset().top - 100) {
-  //     $(".menu-fixed").css("display", "block");
-  //   }
+    if ($(document).scrollTop() >= $("#description").offset().top - 100) {
+      $(".menu-fixed").css("display", "block");
+    }
 
-  //   this.scroll(function () {
+    this.scroll(function () {
 
-  //     var scrollTop = $(document).scrollTop();
-  //     if (scrollTop >= $($(".description")[4]).offset().top - 100) {
-  //       $(".menu-fixed").css("display", "block");
-  //     } else {
-  //       $(".menu-fixed").css("display", "none");
-  //     }
-  //     $(".active").removeClass("active");
-  //     $(".menu-fixed a[href=" + getCurrentElement(scrollTop) + "]").parent().addClass("active");
-  //   });
+      var scrollTop = $(document).scrollTop();
+      if (scrollTop >= $("#description").offset().top - 100) {
+        $(".menu-fixed").css("display", "block");
+      } else {
+        $(".menu-fixed").css("display", "none");
+      }
+      $(".active").removeClass("active");
+      $(".menu-fixed a[href=" + getCurrentElement(scrollTop) + "]").parent().addClass("active");
+    });
 
-  // };
+  };
   // check to active handle for scroll
-  // if ($(".detail").length != 0) {
+  if ($(".detail").length != 0) {
 
-  //   $(document).documentScroll();
-  //   $(".menu-fixed li").setActive();
+    $(document).documentScroll();
+    $(".menu-fixed li").setActive();
 
-  // };
+  };
 
   $(".show-form").click(function (){
     $(".tele-sales").css("display", "none");
@@ -205,69 +205,6 @@ $(document).ready(function () {
     };
     
   });
-
-  // $("#submit-coupon-code").on("click", function () {
-  //   var input_code = $("#coupon-code-container input[type=text]")
-  //   var coupon_code = input_code.val();
-  //   var course_id = input_code.attr("course_id");
-  //   var price = input_code.attr("price");
-  //   var discount = input_code.attr("discount");
-
-  //   if (coupon_code.trim() == "") {
-  //     input_code.focus();
-  //     return;
-  //   }
-  //   if (course_id.trim() == "") {
-  //     return;
-  //   }
-
-  //   var URL = "http://code.pedia.vn/coupon";
-  //   var params = {coupon: coupon_code, q: Math.random()}
-  //   $.ajax({
-  //     type: 'GET',
-  //     url: URL,
-  //     data: params,
-  //     success: function (data, textStatus, xhr) {
-  //       // if (xhr.responseJSON.course_id == course_id) {
-  //       //   discount = parseFloat(discount) + parseFloat(xhr.responseJSON.return_value);
-  //       //   price = (parseInt(price) * (100 - discount) / 100 / 1000) * 1000;
-  //       //   $(".course-price").text(number_to_currency(price, "đ", ","));
-  //       //   $(".discount").text("(Giảm giá: " + discount + "%)");
-
-  //       //   var coupon_code_param = getParameterByName("coupon_code", $("#btn-buy").attr("href"));
-  //       //   if (coupon_code_param == "") {
-  //       //     $("#btn-buy").attr("href", $("#btn-buy").attr("href") + "&coupon_code=" + coupon_code);
-  //       //   } else if (coupon_code_param.split(",").indexOf(coupon_code) < 0) {
-  //       //     $("#btn-buy").attr("href", $("#btn-buy").attr("href") + "," + coupon_code);
-  //       //   };
-  //       // } else {
-  //       //   $("#coupon-code-container input[type=text]").val("Mã coupon không tồn tại");
-  //       // };
-  //     },
-  //     complete: function (xhr, textStatus) {
-  //       if (xhr.responseJSON.course_id == course_id) {
-  //         discount = parseFloat(discount) + parseFloat(xhr.responseJSON.return_value);
-  //         price = parseInt((parseInt(price) * (100 - discount) / 100 / 1000)) * 1000;
-  //         $(".course-price").text(number_to_currency(price, "đ", ","));
-  //         $(".discount").text("(Giảm giá: " + discount + "%)");
-
-  //         var coupon_code_param = getParameterByName("coupon_code", $("#btn-buy").attr("href"));
-  //         if (coupon_code_param == "") {
-  //           $("#btn-buy").attr("href", $("#btn-buy").attr("href") + "&coupon_code=" + coupon_code);
-  //         } else if (coupon_code_param.split(",").indexOf(coupon_code) < 0) {
-  //           $("#btn-buy").attr("href", $("#btn-buy").attr("href") + "," + coupon_code);
-  //         };
-  //         d = new Date(xhr.responseJSON.expired_date);
-          
-  //         $("#expired_date").text("Ngày hết hạn: " + d.getDate() + "/" +(d.getMonth() + 1) + "/" + d.getFullYear());
-  //         $("#coupon-code-container").remove();
-  //         $("#line-through-price").css("display", "block");
-  //       } else {
-  //         $("#coupon-code-container input[type=text]").val("Mã coupon không tồn tại");
-  //       };
-  //     }
-  //   });
-  // });
 
   function number_to_currency(price, unit, delimiter) {
     unit = unit || "đ";
