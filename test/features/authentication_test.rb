90df8488ea2f9b918115273deededb8a327d95de
Nguyễn Phan Hải <hainp2604@gmail.com>
2015-10-06 19:59:03 +0700

    fix: fix build

--- test/features/authentication_test.rb
+++ test/features/authentication_test.rb
@@ -10,14 +10,12 @@ feature 'Authentication' do
   scenario 'the first test' do
     visit '/'
 
-    within('header') do
-      find('.btn[data-target="#login-modal"]').click
-    end
+    find('.link-exist-account').click
 
-    within('#login-modal.modal.fade') do
+    within('.form-login') do
       fill_in('user[email]', with: 'nguyendanhtu@tudemy.vn')
       fill_in('user[password]', with: '12345678')
-      find('.btn-login-submit').click
+      find('.btn-login').click
     end
 
     page.must_have_content('Test Category 1')
