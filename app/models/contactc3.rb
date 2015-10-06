8b493fb0117f16c083833012d8f5172a0c49cefa
Nguyen Khac Quang <quangnk@topica.edu.vn>
2015-10-06 17:10:26 +0700

    sua loi nhap contactc3

--- app/models/contactc3.rb
+++ app/models/contactc3.rb
@@ -2,15 +2,15 @@ class Contactc3
   include Mongoid::Document
   include Mongoid::Timestamps
 
-  field :name, type: String
-  field :mobile, type: String
-  field :email, type: String
-  field :course_id, type: String
-  field :course_name, type: String
-  field :msg, type: String
-  field :type, type: String
+  field :name, type: String, default: ''
+  field :mobile, type: String, default: ''
+  field :email, type: String, default: ''
+  field :course_id, type: String, default: ''
+  field :course_name, type: String, default: ''
+  field :msg, type: String, default: ''
+  field :type, type: String, default: ''
 
-  def insert_contactc3
+  def self.insert_contactc3 data
     Contactc3.create(data)
   end
 end
