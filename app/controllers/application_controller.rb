c4518ea1c88375be22e3745d8f1bb3d93a4add57
duongnd <duongnd@topica.edu.vn>
2015-10-07 17:43:03 +0700

    feature course for 404 page

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -182,15 +182,8 @@ class ApplicationController < ActionController::Base
   end
 
   def page_not_found
-
-  end
-
-  def server_error
-    
-  end
-
-  def page_not_found
-
+    labels   = Constants.LabelsValues
+    @courses = Course.all.desc(:students).limit(4).to_a
   end
 
   def server_error
