b65844b47e390256470810bed0e7f0b20c60bec6
luandt <luandt@topica.edu.vn>
2015-11-03 10:07:52 +0700

    test/controllers/users_controller_test.rb

--- test/controllers/users_controller_test.rb
+++ test/controllers/users_controller_test.rb
@@ -3,14 +3,9 @@ require 'test_helper'
 describe 'UsersController' do
 
   before :each do
-    email = 'test9001@gmail.com'
-    token = '9890fc4da44e6a569397d80738f875872adfaafc'
-    @link = 'https://pedia.vn/users/reset_password?token=9890fc4da44e6a569397d80738f875872adfaafc'
     @user = User.create(
       email: 'test9001@gmail.com',
       password: '12345678',
-      reset_password_token: token,
-      reset_password_sent_at: Time.now
       )
   end
 
@@ -20,7 +15,7 @@ describe 'UsersController' do
 
   describe 'POST #forgot_password' do
 
-    # Check
+    # Check email input
     it 'should return message when email blank' do
       post :forgot_password
 
@@ -31,8 +26,6 @@ describe 'UsersController' do
     # Check user valid use email input
     it 'should return 200 when email valid' do
       stub_request(:post, 'http://email.pedia.vn/email_services/send_email').to_return(:status => 200, :body => '', :headers => {})
-
-      post :forgot_password, email: @user.email, reset_password_token: @user.reset_password_token,reset_password_sent_at: @user.reset_password_sent_at
 
       assert_response 200
     end
